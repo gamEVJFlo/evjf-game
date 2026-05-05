37dd179cc60e5763727a36dd95517e5a499ed593
gamEVJFlo <eloyolo@hotmail.fr>
2026-05-05 22:37:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function playSound() {
     sound.play();
   }
 }
+
+function addPlayer() {
+  const div = document.createElement("div");
+  div.className = "player-block";
+
+  div.innerHTML = `
+    <input type="text" name="equipe[]" placeholder="Nom / prénom / surnom">
+  `;
+
+  document.getElementById("team").appendChild(div);
+}
