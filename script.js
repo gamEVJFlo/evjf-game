a7857db31fab052ae831206749cd936aec369bbc
gamEVJFlo <eloyolo@hotmail.fr>
2026-05-06 07:44:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ function addPlayer() {
 
   document.getElementById("team").appendChild(div);
 }
+
+const videos = [
+  "https://www.youtube.com/embed/VIDEO_ID_1",
+  "https://www.youtube.com/embed/VIDEO_ID_2",
+  "https://www.youtube.com/embed/VIDEO_ID_3"
+];
+
+document.getElementById("funButton").addEventListener("click", () => {
+  const random = videos[Math.floor(Math.random() * videos.length)];
+
+  document.getElementById("videoContainer").innerHTML = `
+    <iframe width="100%" height="300"
+    src="${random}"
+    frameborder="0"
+    allowfullscreen></iframe>
+  `;
+});
+
+setTimeout(() => {
+  document.getElementById("funButton").style.display = "block";
+}, 10000);
